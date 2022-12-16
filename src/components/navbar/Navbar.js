518cb0b5cdf1b3d4b5d3d58a63baf104a1531224
SanderLoman <sanderfeitsma13@gmail.com>
2022-12-16 03:52:33 +0100

    weltruste

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,12 +6,12 @@ import "./Navbar.css"
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
     return (
-        <nav className="border-b text-5xl">
+        <nav className="text-5xl">
             <div className="px-6 py-3">
                 <div className="flex items-center justify-between">
                     <a href="/">
                         <div className="flex items-center">
-                            <span className="text-customPurple mx-2 font-semibold">
+                            <span className="mx-4 font-semibold">
                                 WTF
                             </span>
                             <img
@@ -32,29 +32,50 @@ const Navbar = () => {
                             <FaBars className="sm:hidden" />
                         )}
                     </button>
+                    {/* desktop menu */}
                     <ul className="sm:flex hidden">
                         <li className="flex items-center">
-                            <span className="text-customPurple mx-2 font-semibold">
+                            <span className="mx-4 font-semibold">
                                 <a href="/">Staking</a>
                             </span>
                         </li>
                         <li className="flex items-center">
-                            <span className="text-customPink mx-2 font-semibold">
+                            <span className="mx-4 font-semibold">
                                 <a href="/">Roadmap</a>
                             </span>
                         </li>
                         <li className="flex items-center">
-                            <span className="text-customOrange mx-2 font-semibold">
+                            <span className="mx-4 font-semibold">
                                 <a href="/">Team</a>
                             </span>
                         </li>
                     </ul>
                 </div>
             </div>
+            {/* mobile menu */}
+            <ul
+                className={`sm:hidden ${
+                    isOpen ? "block" : "hidden"
+                } relative top-full right-0 mt-2 px-4 py-2 bg-white shadow-md z-10`}
+            >
+                <li className="block px-4 py-2">
+                    <a href="/" className="flex justify-center font-semibold">
+                        Staking
+                    </a>
+                </li>
+                <li className="block px-4 py-2">
+                    <a href="/" className="flex justify-center font-semibold">
+                        Roadmap
+                    </a>
+                </li>
+                <li className="block px-4 py-2">
+                    <a href="/" className="flex justify-center font-semibold">
+                        Team
+                    </a>
+                </li>
+            </ul>
         </nav>
     )
 }
 
 export default Navbar
-
-// kijk de video in discord weer verder 
