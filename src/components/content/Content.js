9ae3d702f083ed98619c9bf1b942f05c9117e82b
SanderLoman <sanderfeitsma13@gmail.com>
2022-12-20 22:25:18 +0100

    je moeder

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -4,12 +4,12 @@ import "./Content.css"
 
 const Content = () => {
     return (
-        <div className="content-container flex md:flex-row items-center xl:m-52">
-            <div className="content-left flex flex-col items-center">
+        <div className="content-container flex md:flex-row items-center w-screen h-screen -mt-40 md:-mt-96">
+            <div className="flex flex-col items-center m-4">
                 <h1 className="xl:text-10xl text-5xl font-bold text-center">
                     WTF DUDES CRYPTO
                 </h1>
-                <p className="xl:text-5xl text-3xl text-center mt-4 mb-16 md:w-3/4">
+                <p className="xl:text-5xl text-3xl text-center mt-4 mb-16 md:w-2/4">
                     wtf dudes crypto is a token made for the people who suffered
                     in this rough bear market. A lot of you guys probably lost
                     some of your gains because of Bitcoin... Like WTF Bitcoin?!
@@ -18,18 +18,18 @@ const Content = () => {
                     of things prepared so hop in and WTF DUDES this is about to
                     get crazy!
                 </p>
-                <div className="flex text-5xl justify-around w-full">
-                    <div className="flex">
-                        <img src={Check} alt="Check" className="" />
-                        safe team
+                <div className="flex flex-row items-center md:flex-row md:text-5xl text-2xl justify-around w-full md:w-2/4">
+                    <div className="flex items-center">
+                        <img src={Check} alt="Check" className="h-6 md:h-auto"/>
+                        <p className="">Safe team</p>
                     </div>
-                    <div className="flex">
-                        <img src={Check} alt="Check" className="" />
-                        utility
+                    <div className="flex items-center">
+                        <img src={Check} alt="Check" className="h-6 md:h-auto"/>
+                        <p className="">Utility</p>
                     </div>
-                    <div className="flex">
-                        <img src={Check} alt="Check" className="" />
-                        community
+                    <div className="flex items-center">
+                        <img src={Check} alt="Check" className="h-6 md:h-auto"/>
+                        <p className="">Community</p>
                     </div>
                 </div>
             </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
     return (
         <div className="header-container flex flex-col md:flex-row items-center justify-evenly h-screen px-4 pb-8">
             <div className="header-left flex flex-col items-center">
-                <img src={HeaderLogo} alt="Header logo" className="pb-8" />
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target="_blank"><img src={HeaderLogo} alt="Header logo" className="pb-8 md:transform md:transition-all md:hover:scale-105" /></a>
                 <div className="header-links flex w-full items-center justify-evenly">
                     <a href="https://app.uniswap.org/#/swap" target="_blank">
                         <img
@@ -25,7 +25,7 @@ const Header = () => {
                             alt="Header link 2"
                             className="md:transform md:transition-all md:hover:scale-125"
                         />
-                    </a>{" "}
+                    </a>
                     {/* Change later link is not right */}
                     <a href="https://t.me/Sanduhh" target="_blank">
                         <img
@@ -33,7 +33,7 @@ const Header = () => {
                             alt="Header link 3"
                             className="md:transform md:transition-all md:hover:scale-125"
                         />
-                    </a>{" "}
+                    </a>
                     {/* Change later link is not right */}
                 </div>
             </div>
