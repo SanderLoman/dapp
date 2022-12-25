eab39ef36da51da57ea959891fd752b809d94eb6
SanderLoman <sanderfeitsma13@gmail.com>
2022-12-26 00:46:57 +0100

    123sadasd123123

--- src/components/roadmap/Roadmap.js
+++ src/components/roadmap/Roadmap.js
@@ -5,11 +5,11 @@ const Roadmap = () => {
     return (
         <div className="flex flex-col h-screen">
             <div className="flex flex-col justify-center text-center h-1/6 w-full">
-                <h1 className="text-5xl md:text-15xl">Roadmap</h1>
-                <p className="text-4xl md:text-5xl">With the right fucking plan, great things can happen!</p>
+                <h1 className="text-8xl">Roadmap</h1>
+                <p className="text-5xl">With the right fucking plan, great things can happen!</p>
             </div>
             <div className="flex justify-center items-center h-5/6 w-full">
-                <img src={RoadM} alt="Roadmap" className="" />
+                <img src={null} alt="Roadmap" className="" />
             </div>
         </div>
     )
--- src/components/staking/Staking.js
+++ src/components/staking/Staking.js
@@ -8,19 +8,28 @@ const Staking = () => {
     return (
         <div className="main-container flex justify-evenly">
             <div className="image-container flex justify-center items-center w-1/3">
-                <img src={dudes2} alt="Dudes" className="" />
+                <Slide left>
+                    <img src={dudes2} alt="Dudes" className="" />
+                </Slide>
             </div>
             <div className="text-container flex flex-col justify-center items-center w-1/3">
-                <h1 className="text-12xl">Staking</h1>
-                <p className="text-5xl">
-                    You can now stake you WTF tokens! We want our investors to
-                    chill and earn. So sit back, relax.. And enjoy your rewards!
-                </p>
-                <img
-                    src={HeaderButton}
-                    alt="Staking button"
-                    className="button w-1/5"
-                />
+                <Fade bottom>
+                    <h1 className="text-12xl">Staking</h1>
+                </Fade>
+                <Fade bottom>
+                    <p className="text-5xl">
+                        You can now stake you WTF tokens! We want our investors
+                        to chill and earn. So sit back, relax.. And enjoy your
+                        rewards!
+                    </p>
+                </Fade>
+                <Fade bottom>
+                    <img
+                        src={HeaderButton}
+                        alt="Staking button"
+                        className="button w-1/5"
+                    />
+                </Fade>
             </div>
         </div>
     )
